88840a64dc8b2a06764e077f169a456781b82bfd
Gerardo-rgf <70921927+Gerardo-rgf@users.noreply.github.com>
2022-12-19 12:57:09 -0600

    [ADD] Add custom addons
    readonly_unit_price and sales_order_delivery_status and stock_no_negative

---
+++ itechgroup_readonly_unit_price/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    "name": "Read only unit price | ALL IN ONE",
+    "summary": """Using this module you can make read only unit price sale, purchase and invoice order line.
+        | Simple to deal with the unit price | You can read only access to a specific users, everyone can't edit
+        the unit price in the sales order, invoice and purchase order | No special configuration is needed, just install it and use it
+        | This module will save you significant time, it lessens human exertion | This module is compatible with one of them or all modules
+        | Read-only Unit Price | Unit Price Readonly | Price Unit Readonly | Sale and Invoice Unit Price Readonly | Read only Unit Price | Read only Sale and Invoice Order line
+        | Sale Order Price Unit Readonly | Sale Price Readonly
+    """,
+    "description": """
+        Using this module you can make read only unit price sale, purchase and invoice order line.
+        | Simple to deal with the unit price | You can read only access to a specific users, everyone can't edit
+        the unit price in the sales order, invoice and purchase order | No special configuration is needed, just install it and use it
+        | This module will save you significant time, it lessens human exertion | This module is compatible with one of them or all modules
+    """,
+    "category": "Extra Tools",
+    "version": "15.0.0.1",
+    "sequence": 3,
+    "author": "Itechgroup",
+    "depends": ['base'],
+    "live_test_url": "http://ec2-18-191-155-154.us-east-2.compute.amazonaws.com:8069/",
+    "support": "support@itechgroup.info",
+    "data": [
+        "security/readonly_field_security.xml"
+    ],
+    "qweb": [],
+    "license": "OPL-1",
+    "images": ['static/description/banner.gif'],
+    "price": '15',
+    "currency": "EUR",
+    "installable": True,
+    "application": True
+}
