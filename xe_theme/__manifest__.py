515886c982d4419a79c41ecd9bcb39e8705f95ed
David Morales <dmoralesh1989@gmail.com>
2024-07-11 11:27:39 -0600

    [REF] assets_frontend

--- xe_theme/__manifest__.py
+++ xe_theme/__manifest__.py
@@ -13,7 +13,7 @@
         "web",
     ],
     'assets': {
-        'web.assets_common': [
+        'web.assets_frontend': [
             'xe_theme/static/src/css/*.css',
             'xe_theme/static/src/js/*.js',
             'xe_theme/static/src/scss/*.scss',
