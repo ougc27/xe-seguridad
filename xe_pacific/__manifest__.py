24b7e9cfdb0a1ec80ca34cfe7783f86351a81641
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-09-10 14:33:05 -0600

    [ADD] Added modules for pacific migration

---
+++ xe_pacific/__manifest__.py
@@ -0,0 +1,29 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "MX-EDI Pacific Rim",
+    "version": "17.0.0.0.0",
+    "license": "LGPL-3",
+    "author": "XE Brands",
+    'sequence': 1,
+    "depends": [
+        'base',
+        'sale_project',
+        'helpdesk_fsm',
+        'stock'
+    ],
+    "category": "Accounting",
+    "summary": "MX-EDI XE Brands",
+    "data": [],
+    #'assets': {
+        #'web.assets_backend': [
+            #'xe_pacific/static/src/js/attachment_preview.js',
+        #],
+        #'web.assets_qweb': [
+            #'xe_pacific/static/src/xml/attachment_preview.xml',
+        #],
+    #},
+    "demo": [],
+    'installable': True,
+    'auto_install': False,
+}
