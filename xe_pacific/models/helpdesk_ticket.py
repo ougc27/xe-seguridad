959df5b952f8f855c756a647b2050789bd8093a9
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-09-11 16:19:18 -0600

    [ADD] Added module for restrict sales in pos without stock

--- xe_pacific/models/helpdesk_ticket.py
+++ xe_pacific/models/helpdesk_ticket.py
@@ -1,58 +1,105 @@
-from odoo import models, fields, api, _
+import logging
+from collections import defaultdict
+from odoo import models, api, fields, Command
 
+_logger = logging.getLogger(__name__)
 
-class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
-    name = fields.Char(
-        compute="_compute_ticket_type_name",
-        string='Subject',
-        index=True,
-        tracking=True
-    )
+    is_exception = fields.Boolean(string="Excepción")
 
-    ticket_type = fields.Selection([
-        ('administration', 'Administración'),
-        ('reinstallation', 'Albañilería / Reinstalación'),
-        ('information', 'Asesoría / Información'),
-        ('ci_change', 'Cambio Ci'),
-        ('door_change', 'Cambio de puerta'),
-        ('return', 'Devolución / Cancelación'),
-        ('survey', 'Encuesta'),
-        ('delivery', 'Entrega'),
-        ('wrong_part', 'Faltante / Pieza equivocada'),
-        ('consumption_output', 'Salida por consumo'),
-        ('installation', 'Instalación'),
-        ('mechanism', 'Mecanismo'),
-        ('other', 'Otro'),
-        ('paint', 'Pinura'),
-        ('complaint', 'Queja'),
-        ('sale', 'Venta'),
-        ('technical_visit', 'VT'),
-    ], required=True)
+    def mark_tasks_as_exception(self):
+        for line in self.order_line:
+            task_stage = self.env['project.task.type'].sudo().search([
+                ('name', 'ilike', '%EXCEPCIÓN'),
+                ('project_ids', '=', line.task_id.project_id.id)
+            ], limit=1)
+            if task_stage:
+                line.task_id.write({
+                    'stage_id': task_stage.id
+                })
 
-    def action_generate_fsm_task(self):
-        self.ensure_one()
-        if not self.partner_id:
-            self.partner_id = self._find_or_create_partner(self.partner_name, self.partner_email, self.company_id.id)
-        return {
-            'type': 'ir.actions.act_window',
-            'name': _('Create a Field Service task'),
-            'res_model': 'helpdesk.create.fsm.task',
-            'view_mode': 'form',
-            'target': 'new',
-            'context': {
-                'use_fsm': True,
-                'default_helpdesk_ticket_id': self.id,
-                'default_user_id': False,
-                'default_partner_id': self.partner_id.id,
-                'default_name': self.display_name,
-                'default_project_id': self.team_id.fsm_project_id.id,
-                'dialog_size': 'medium',
-            }
-        }
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self:
+            _logger.info("entre en el write")
+            if rec.is_exception and rec.state == 'sale':
+                rec.mark_tasks_as_exception()
+        return res
 
-    @api.depends('name', 'ticket_type')
-    def _compute_ticket_type_name(self):
-        for ticket in self:
-            ticket.name = dict(self._fields['ticket_type'].selection).get(ticket.ticket_type)
+    def _action_confirm(self):
+        result = super()._action_confirm()
+        for rec in self:
+            if rec.team_id.name == 'CONSTRUCTORAS':
+                for line in rec.order_line:
+                    sku = line.product_id.default_code
+                    if sku in ('INS10', 'INSBASCDI', 'VISREF', 'VISTEC_COMPRADA'):
+                        project_id = self.env['project.project'].search(
+                            [('name', '=', 'E.- INSTALACIONES CONSTRUCTORAS')]).id
+                        line.task_id.write({
+                            'sale_order_id': rec.id,
+                            'project_id': project_id,
+                            'sale_line_id': line.id,
+                            'partner_id': rec.partner_id.id 
+                        })
+            else:
+                for line in rec.order_line:
+                    line.task_id.write({
+                        'sale_order_id': rec.id,
+                    })
+        #if rec.is_exception:
+                """for line in rec.order_line:
+                    task_stage = self.env['project.task.type'].sudo().search([
+                        ('name', 'ilike', '%EXCEPCIÓN'),
+                        ('project_ids', '=', line.task_id.project_id.id)
+                    ], limit=1)
+                    if task_stage:
+                        line.task_id.write({
+                            'stage_id': task_stage.id
+                        })"""
+                #rec.mark_tasks_as_exception()
+        return result
+
+    @api.depends('order_line.product_id.project_id', 'order_line.project_id')
+    def _compute_tasks_ids(self):
+        tasks_per_so = self.env['project.task']._read_group(
+            domain=['&', ('project_id', '!=', False), '|', ('sale_line_id', 'in', self.order_line.ids), ('sale_order_id', 'in', self.ids)],
+            groupby=['sale_order_id'],
+            aggregates=['id:recordset', '__count']
+        )
+        so_with_tasks = self.env['sale.order']
+        for order, tasks_ids, tasks_count in tasks_per_so:
+            if order:
+                order.tasks_ids = tasks_ids
+                order.tasks_count = tasks_count
+                so_with_tasks += order
+            else:
+                # tasks that have no sale_order_id need to be associated with the SO from their sale_line_id
+                for task in tasks_ids:
+                    task_so = task.sale_line_id.order_id
+                    task_so.tasks_ids = [Command.link(task.id)]
+                    task_so.tasks_count += 1
+                    so_with_tasks += task_so
+        remaining_orders = self - so_with_tasks
+        if remaining_orders:
+            remaining_orders.tasks_ids = [Command.clear()]
+            remaining_orders.tasks_count = 0
+
+    @api.depends('order_line.product_id', 'order_line.project_id')
+    def _compute_project_ids(self):
+        is_project_manager = self.user_has_groups('project.group_project_manager')
+        projects = self.env['project.project'].search([('sale_order_id', 'in', self.ids)])
+        projects_per_so = defaultdict(lambda: self.env['project.project'])
+        for project in projects:
+            projects_per_so[project.sale_order_id.id] |= project
+        for order in self:
+            projects = order.order_line.mapped('task_id.project_id')
+            if not projects:
+                projects |= order.order_line.mapped('product_id.project_id')
+            projects |= order.project_id
+            projects |= projects_per_so[order.id or order._origin.id]
+            if not is_project_manager:
+                projects = projects._filter_access_rules('read')
+            order.project_ids = projects
+            order.project_count = len(projects)
