34843b9176f356745358c60cdea9fb8553de350b
Omar Mejia <topesa@gmail.com>
2024-09-17 15:35:09 -0700

    [FIX][TYPO] addenda_morwi_ace_hardware

--- addenda_morwi_ace_hardware/models/account_move.py
+++ addenda_morwi_ace_hardware/models/account_move.py
@@ -7,7 +7,7 @@ class AccountMove(models.Model):
     
     @api.onchange('purchase_order_reference')
     def _onchange_purchase_order_reference(self):
-        addenda_id = self.env.ref('addenda_morwi_ace_hadware.l10n_mx_edi_addenda_ace_hardware')
+        addenda_id = self.env.ref('addenda_morwi_ace_hardware.l10n_mx_edi_addenda_ace_hardware')
         if self.purchase_order_reference and self.partner_id.l10n_mx_edi_addenda:
             if self.partner_id.l10n_mx_edi_addenda.id == addenda_id.id:
                 if not self.purchase_order_reference.isnumeric():
