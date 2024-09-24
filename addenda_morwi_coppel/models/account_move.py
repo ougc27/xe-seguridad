d5358b5c1215bee61e1a2cc7f8f375d5faaea1a0
David Morales <dmoralesh1989@gmail.com>
2024-09-24 10:18:44 -0600

    [ADD] merge modules with main code

---
+++ addenda_morwi_coppel/models/account_move.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, fields
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    
+    addenda_coppel_po_date = fields.Date(string='PO Date')
+    addenda_coppel_po_date_delivery = fields.Date(string='PO Date Delivery')
+    addenda_coppel_transaction_type = fields.Char(string='Transaction Type')
+    addenda_coppel_supplier_type = fields.Char(default='1', string='Supplier Type', readonly=True)
+    addenda_coppel_dc = fields.Selection([
+        ('30001', 'CULIACAN'),
+        ('30002', 'LEON'),
+        ('30003', 'HERMOSILLO'),
+        ('30004', 'LAGUNA'),
+        ('30006', 'MONTERREY'),
+        ('30007', 'GUADALAJARA'),
+        ('30008', 'AZCAPOTZALCO'),
+        ('30009', 'PUEBLA'),
+        ('30010', 'VILLA HERMOSA'),
+        ('30011', 'IZTAPALAPA'),
+        ('30012', 'IMP_MXL2'),
+        ('30013', 'MEXICALI'),
+        ('30014', 'IZCALLI'),
+        ('30015', 'IXTAPALUCA'),
+        ('30016', 'TECAMAC'),
+        ('30017', 'MERIDA'),
+        ('30018', 'LOS MOCHIS'),
+        ('30019', 'VERACRUZ'),
+        ('30020', 'GUADALAJARA II'),
+        ('30021', 'IXTAPALUCA IMP'),
+        ('30022', 'TOLUCA'),
+        ('30023', 'GUADALUPE'),
+        ('30024', 'TEXCOCO'),
+        ('30025', 'TIJUANA'),
+        ('30030', 'TECAMAC II'),
+        ('30031', 'PUEBLA II'),
+    ], string='Final C.D. for Delivery')
+    addenda_coppel_reception_dc = fields.Selection([
+        ('30001', 'CULIACAN'),
+        ('30002', 'LEON'),
+        ('30003', 'HERMOSILLO'),
+        ('30004', 'LAGUNA'),
+        ('30006', 'MONTERREY'),
+        ('30007', 'GUADALAJARA'),
+        ('30008', 'AZCAPOTZALCO'),
+        ('30009', 'PUEBLA'),
+        ('30010', 'VILLA HERMOSA'),
+        ('30011', 'IZTAPALAPA'),
+        ('30012', 'IMP_MXL2'),
+        ('30013', 'MEXICALI'),
+        ('30014', 'IZCALLI'),
+        ('30015', 'IXTAPALUCA'),
+        ('30016', 'TECAMAC'),
+        ('30017', 'MERIDA'),
+        ('30018', 'LOS MOCHIS'),
+        ('30019', 'VERACRUZ'),
+        ('30020', 'GUADALAJARA II'),
+        ('30021', 'IXTAPALUCA IMP'),
+        ('30022', 'TOLUCA'),
+        ('30023', 'GUADALUPE'),
+        ('30024', 'TEXCOCO'),
+        ('30025', 'TIJUANA'),
+        ('30030', 'TECAMAC II'),
+        ('30031', 'PUEBLA II'),
+    ], string='C.D. for Reception')
+    addenda_coppel_region = fields.Selection([
+        ('1', 'La Paz, Tijuana, Mexicali'),
+        ('2', 'Culiacán, Hermosillo, Los Mochis'),
+        ('3', 'Laguna'),
+        ('4', 'Monterrey'),
+        ('5', 'Guadalajara'),
+        ('6', 'León, Izcalli'),
+        ('7', 'Puebla'),
+        ('8', 'Villahermosa, Merida'),
+        ('9', 'México, Iztapalapa, Izcalli, Ixtapaluca, Tecamac'),
+    ], string='Region ID')
+    addenda_coppel_currency_function = fields.Selection([
+        ('BILLING_CURRENCY', 'Divisa de facturación'),
+        ('PRICE_CURRENCY', 'Divisa del precio'),
+        ('PAYMENT_CURRENCY', 'Divisa de pago'),
+    ], string='Currency Function')
+    addenda_coppel_packing = fields.Selection([
+        ('SELLER_PROVIDED', 'Caja Propia'),
+        ('BUYER_PROVIDED', 'Caja de Coppel'),
+    ], string='Packing')
+    addenda_coppel_transporter = fields.Char(string='Transporter')
+    addenda_coppel_discount_charge = fields.Selection([
+        ('ALLOWANCE_GLOBAL', 'Descuento Global'),
+        ('CHARGE_GLOBAL', 'Cargo Global'),
+    ], string='Discount or Charge')
+    addenda_coppel_discount_charge_imputation = fields.Selection([
+        ('BILL_BACK', 'Reclamación'),
+        ('OFF_INVOICE', 'Fuera de factura'),
+    ], string='Discount or Charge Imputation')
+    addenda_coppel_discount_type = fields.Selection([
+        ('AA', 'Abono por Publicidad'),
+        ('EAB', 'Descuento por pronto pago'),
+        ('DXDIST', 'Descuento por distribuir la mercancia de bodega a tienda'),
+        ('DXALM', 'Descuento por resguardar la mercancia'),
+        ('DXECEN', 'Descuento por entregar en una sola bodega'),
+        ('DXDIST', 'Descuento por distribuir la mercancía de bodega a tienda'),
+        ('DXALM', 'Descuento por resguardar la mercancía'),
+        ('XECEN', 'Descuento por entregar en una sola bodega'),
+    ], string='Discount Type')
--- xe_customs/models/sale_order.py
+++ xe_customs/models/sale_order.py
@@ -15,6 +15,18 @@ class SaleOrderLine(models.Model):
         compute='_compute_client_barcode',
         store=True)
 
+    down_payment_context = fields.Monetary(
+        string="Reconciled Amount",
+        copy=False
+    )
+    down_payment_ids = fields.One2many(
+        comodel_name='sale.down.payment',
+        inverse_name='order_id',
+        string="Invoices",
+        copy=False,
+        readonly=False
+    )
+
     @api.depends('order_id.partner_id', 'product_id')
     def _compute_client_barcode(self):
         for line in self:
@@ -31,4 +43,199 @@ class SaleOrderLine(models.Model):
             else:
                 line.client_barcode = False
 
-    
+    def action_cancel(self):
+        for order in self:
+            if len(order.down_payment_ids) > 0:
+                raise UserError(_('You cannot cancel an order with down payments.'))
+            else:
+                return super(SaleOrder, order).action_cancel()
+
+    def action_confirm(self):
+        for order in self:
+            res = super(SaleOrder, order).action_confirm()
+            order._add_client_to_product()
+            return res
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    margin = fields.Monetary("Margin", compute='_compute_margin', store=True, groups="account.group_account_manager")
+
+    client_barcode = fields.Char(
+        string='Client Product Barcode',
+        compute='_compute_client_barcode',
+        store=True)
+
+    @api.depends('order_id.partner_id', 'product_id')
+    def _compute_client_barcode(self):
+        for line in self:
+            if not line.product_id or not line.order_id.partner_id:
+                line.client_barcode = False
+                return
+            client = line.product_id._select_client(
+                partner_id=line.order_id.partner_id,
+                quantity=line.product_uom_qty,
+                date=line.order_id.date_order and line.order_id.date_order.date(),
+                uom_id=line.product_uom)
+            if client:
+                line.client_barcode = client.product_barcode
+            else:
+                line.client_barcode = False
+
+    def _prepare_client_info(self, partner, line, price, currency):
+        # Prepare clientinfo data when adding a product
+        return {
+            'name': partner.id,
+            'sequence': max(line.product_id.client_ids.mapped('sequence')) + 1 if line.product_id.client_ids else 1,
+            'min_qty': 0.0,
+            'price': price,
+            'currency_id': currency.id,
+            'delay': 0,
+        }
+
+    @api.onchange('product_id', 'product_uom_qty')
+    def onchange_product_id(self):
+        for line in self:
+            if not line.product_id:
+                return
+
+            client = line.product_id._select_client(
+                partner_id=line.order_id.partner_id,
+                quantity=line.product_uom_qty,
+                date=line.order_id.date_order and line.order_id.date_order.date(),
+                uom_id=line.product_uom)
+            if client:
+                line.price_unit = client.price
+                name = ""
+                if client.product_code:
+                    name += "[%s] " % client.product_code
+                elif line.product_id.default_code:
+                    name += "[%s] " % line.product_id.default_code
+                if client.product_name:
+                    name += client.product_name
+                else:
+                    name += line.product_id.name
+                line.name = name
+            else:
+                line.price_unit = line.product_id.lst_price
+                line.name = line.product_id.name
+
+
+class SaleDownPayment(models.Model):
+    _name = "sale.down.payment"
+    _description = "Sale Down Payment"
+
+    invoice_id = fields.Many2one(
+        comodel_name = 'account.move',
+        string = "Invoice",
+        copy = False
+    )
+    l10n_mx_edi_cfdi_uuid = fields.Char(
+        string = "Fiscal Folio",
+        related = 'invoice_id.l10n_mx_edi_cfdi_uuid',
+    )
+    order_line_id = fields.Many2one(
+        comodel_name = 'sale.order.line',
+        string = "Order Line",
+        copy = False
+    )
+    order_id = fields.Many2one(
+        comodel_name = 'sale.order',
+        string = "Order",
+        related = 'order_line_id.order_id',
+        copy = False
+    )
+    currency_id = fields.Many2one(
+        comodel_name = 'res.currency',
+        string = "Currency",
+        related = 'invoice_id.currency_id',
+    )
+    amount = fields.Monetary(
+        string = "Amount",
+        copy = False
+    )
+
+    @api.onchange('invoice_id')
+    def _onchange_invoice_id(self):
+        for payment in self:
+            if len(payment._origin.invoice_id) == 0 and len(payment.invoice_id) > 0:
+                payment.amount = payment.invoice_id.reconcile_balance
+            if len(payment._origin.invoice_id) > 0 and payment.invoice_id.id != payment._origin.invoice_id.id:
+                raise UserError(_('You cannot change the invoice of a down payment.'))
+
+    @api.onchange('amount')
+    def _onchange_amount(self):
+        for payment in self:
+            if payment.amount and payment.amount != payment._origin.amount:
+                order = payment.order_id
+                tax_id = order.order_line.tax_id
+                amount = payment.amount / (1 + (tax_id[0].amount / 100))
+
+                if payment.order_line_id:
+                    payment.order_line_id.price_unit = amount
+                    payment.order_line_id.invoice_lines.price_unit = amount
+                else:
+                    # Create down payment section if necessary
+                    section = self.env['sale.order.line'].with_context(sale_no_log_for_new_lines=True)
+                    if not any(line.display_type and line.is_downpayment for line in order.order_line):
+                        section.create(
+                            self._prepare_down_payment_section_values(order)
+                        )
+
+                    # Add new down payment line on Invoice
+                    invoice_down_payment = self.env['account.move.line'].create({
+                        'move_id': payment.invoice_id.id,
+                        'product_id': order.company_id.sale_down_payment_product_id.id,
+                        'quantity': 0,
+                        'price_unit': amount,
+                        'tax_ids': [(6, 0, tax_id.ids)],
+                        'is_downpayment': True,
+                        'name': _('Down Payment'),
+                        'sequence': payment.invoice_id.invoice_line_ids and payment.invoice_id.invoice_line_ids[-1].sequence + 1 or 10,
+                    })
+
+                    # Add new down payment line on Sale
+                    down_payment = order.order_line.create({
+                        'product_id': order.company_id.sale_down_payment_product_id.id,
+                        'order_id': order._origin.id,
+                        'product_uom_qty': 0,
+                        'discount': 0.0,
+                        'price_unit': amount,
+                        'tax_id': [(6, 0, tax_id.ids)],
+                        'is_downpayment': True,
+                        'invoice_lines': [(6, 0, invoice_down_payment.ids)],
+                        'sequence': order.order_line and order.order_line[-1].sequence + 1 or 10,
+                    })
+                    payment.order_line_id = down_payment
+                    invoice_down_payment.sale_line_ids += down_payment
+                    payment.invoice_id._get_source_orders()
+
+    def unlink(self):
+        for payment in self:
+            payment.order_line_id.invoice_lines.write({
+                'sale_line_ids': False,
+                'name': _('Unlinked Down Payment'),
+            })
+            payment.order_line_id.write({
+                'invoice_lines': False,
+                'price_unit': 0,
+                'name': _('Deleted Down Payment'),
+            })
+        return super(SaleDownPayment, self).unlink()
+
+    def _prepare_down_payment_section_values(self, order):
+        context = {'lang': order.partner_id.lang}
+
+        so_values = {
+            'name': _('Down Payments'),
+            'product_uom_qty': 0.0,
+            'order_id': order._origin.id,
+            'display_type': 'line_section',
+            'is_downpayment': True,
+            'sequence': order.order_line and order.order_line[-1].sequence + 1 or 10,
+        }
+
+        del context
+        return so_values
+  
