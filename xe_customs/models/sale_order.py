985b46c2de0a603b763ecb6eaad1c1326f8567e2
David Morales <dmoralesh1989@gmail.com>
2024-09-24 10:42:31 -0600

    [REF] sale_order

--- xe_customs/models/sale_order.py
+++ xe_customs/models/sale_order.py
@@ -2,18 +2,12 @@
 # © 2024 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
+from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    margin = fields.Monetary("Margin", compute='_compute_margin', store=True, groups="account.group_account_manager")
-
-    client_barcode = fields.Char(
-        string='Client Product Barcode',
-        compute='_compute_client_barcode',
-        store=True)
 
     down_payment_context = fields.Monetary(
         string="Reconciled Amount",
@@ -27,21 +21,41 @@ class SaleOrder(models.Model):
         readonly=False
     )
 
-    @api.depends('order_id.partner_id', 'product_id')
-    def _compute_client_barcode(self):
-        for line in self:
-            if not line.product_id or not line.order_id.partner_id:
-                line.client_barcode = False
-                return
-            client = line.product_id._select_client(
-                partner_id=line.order_id.partner_id,
-                quantity=line.product_uom_qty,
-                date=line.order_id.date_order and line.order_id.date_order.date(),
-                uom_id=line.product_uom)
-            if client:
-                line.client_barcode = client.product_barcode
-            else:
-                line.client_barcode = False
+    def _add_client_to_product(self):
+        # Add the partner in the client list of the product if the client is not registered for
+        # this product. We limit to 10 the number of clients for a product to avoid the mess that
+        # could be caused for some generic products ("Miscellaneous").
+        for line in self.order_line:
+            # Do not add a contact as a client
+            partner = line.order_id.partner_id if not line.order_id.partner_id.parent_id else line.order_id.partner_id.parent_id
+            already_client = (partner | line.order_id.partner_id) & line.product_id.client_ids.mapped('name')
+            if line.product_id and not already_client and len(line.product_id.client_ids) <= 10:
+                # Convert the price in the right currency.
+                currency = partner.property_purchase_currency_id or line.env.company.currency_id
+                price = line.currency_id._convert(line.price_unit, currency, line.company_id, line.order_id.date_order or fields.Date.today(), round=False)
+                # Compute the price for the template's UoM, because the client's UoM is related to that UoM.
+                if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
+                    default_uom = line.product_id.product_tmpl_id.uom_po_id
+                    price = line.product_uom._compute_price(price, default_uom)
+
+                clientinfo = line._prepare_client_info(partner, line, price, currency)
+                # In case the order partner is a contact address, a new clientinfo is created on
+                # the parent company. In this case, we keep the product name and code.
+                client = line.product_id._select_client(
+                    partner_id=line.order_id.partner_id,
+                    quantity=line.product_uom_qty,
+                    date=line.order_id.date_order and line.order_id.date_order.date(),
+                    uom_id=line.product_uom)
+                if client:
+                    clientinfo['product_name'] = client.product_name
+                    clientinfo['product_code'] = client.product_code
+                vals = {
+                    'client_ids': [(0, 0, clientinfo)],
+                }
+                try:
+                    line.product_id.write(vals)
+                except AccessError:  # no write access rights -> just ignore
+                    break
 
     def action_cancel(self):
         for order in self:
