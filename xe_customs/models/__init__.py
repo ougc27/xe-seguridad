7faaab016be8358edfcf3327ed123ef5bcb2e841
David Morales <dmoralesh1989@gmail.com>
2024-09-24 09:59:36 -0600

    [ADD] dependencies

--- xe_customs/models/__init__.py
+++ xe_customs/models/__init__.py
@@ -2,7 +2,6 @@
 # © 2024 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import account_move
 from . import product_clientinfo
 from . import product_product
 from . import product_template
