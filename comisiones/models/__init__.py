a7d8b4aa453b1b65d517ac825b29305d5203f34d
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-04 11:47:24 -0600

    [ADD] Comissions module for salespersons

---
+++ comisiones/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import xe_agente
+from . import res_partner
+from . import sale_order
+from . import account_move
+from . import xe_comisiones
+from . import xe_pago_comisiones
+from . import xe_pago_comisiones_masivo
+from . import account_payment
