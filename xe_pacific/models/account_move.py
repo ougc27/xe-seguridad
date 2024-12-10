da77a90b8727a310418ce9bf4f5a7026128c6be1
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-10 07:52:57 -0600

    [ADD] Remission changes in pacific

--- xe_pacific/models/account_move.py
+++ xe_pacific/models/account_move.py
@@ -16,12 +16,12 @@ class AccountMove(models.Model):
         store=True
     )
 
-    #picking_ids = fields.Many2many(
-        #'stock.picking',
-        #'account_move_picking_rel',
-        #'move_id',
-        #'picking_id',
-        #string='Remissions',
+    picking_ids = fields.Many2many(
+        'stock.picking',
+        'account_move_picking_rel',
+        'move_id',
+        'picking_id',
+        string='Remissions',
         #context={'from_helpdesk_ticket': True},
-        #help='Relationship between referrals and invoice.'
+        help='Relationship between referrals and invoice.'
     #)
