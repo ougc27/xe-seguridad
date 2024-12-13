8deb4500c134c050125824254725105ed86a04b0
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-13 14:19:56 -0600

    [UPD pos_store field in account invoice report

--- xe_pacific/models/account_invoice_report.py
+++ xe_pacific/models/account_invoice_report.py
@@ -4,8 +4,7 @@ from odoo import fields, models
 class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
-    pos_store = fields.Many2one('res.partner', 
-        related='move_id.pos_store',
-        readonly=True,
-        store=True
-    )
+    pos_store = fields.Many2one('res.partner', readonly=True)
+
+    def _select(self):
+        return super()._select() + ", move.pos_store as pos_store"
