c6951f9f6a688ddfcf98999dc46d6f5da324749c
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-18 00:44:32 -0600

    [UPD] Commented part of code

--- xe_pacific/models/stock_picking.py
+++ xe_pacific/models/stock_picking.py
@@ -6,9 +6,9 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    shipping_assignment = fields.Selection([
-        ('logistics', 'Logística'),
-        ('shipments', 'Embarques')], store=True, compute="_compute_shipping_assignment")
+    #shipping_assignment = fields.Selection([
+        #('logistics', 'Logística'),
+        #('shipments', 'Embarques')], store=True, compute="_compute_shipping_assignment")
 
     kanban_task_status = fields.Selection([
         ('to_scheduled', 'POR PROGRAMAR'),
@@ -28,37 +28,37 @@ class StockPicking(models.Model):
         ('finished', 'FINALIZADO'),
         ('exception', 'EXCEPCIÓN')], tracking=True, default='to_scheduled', group_expand='_group_expand_status')
 
-    x_studio_canal_de_distribucin = fields.Char(
-        string="Nombre del Equipo de Ventas",
-        related='group_id.sale_id.partner_id.team_id.name',
-        store=True,
-        readonly=True
-    )
+    #x_studio_canal_de_distribucin = fields.Char(
+        #string="Nombre del Equipo de Ventas",
+        #related='group_id.sale_id.partner_id.team_id.name',
+        #store=True,
+        #readonly=True
+    #)
 
     is_loose_construction = fields.Boolean()
 
-    @api.depends('location_id', 'move_ids', 'x_studio_canal_de_distribucin')
-    def _compute_shipping_assignment(self):
-        for rec in self:
-            if rec.company_id.id == 4:
-                if rec.x_studio_canal_de_distribucin == 'INTERGRUPO':
-                    rec.shipping_assignment = 'shipments'
-                    continue
-                if rec.location_id and rec.location_id.warehouse_id.name:
-                    if any(keyword in rec.location_id.warehouse_id.name for keyword in ['Amazon', 'MercadoLibre']):
-                        rec.shipping_assignment = 'shipments'
-                        continue
-                    if rec.location_id.warehouse_id.name == 'Monterrey PR': 
-                        if rec.x_studio_canal_de_distribucin == 'DISTRIBUIDORES':
-                            rec.shipping_assignment = 'shipments'
-                            continue
-                        if rec.move_ids.filtered(lambda record: record.product_id.default_code == 'FLTENVIO'):
-                            rec.shipping_assignment = 'shipments'
-                            continue
-                rec.shipping_assignment= 'logistics'
-                continue
-            else:
-                rec.shipping_assignment = 'shipments'
+    # @api.depends('location_id', 'move_ids', 'x_studio_canal_de_distribucin')
+    # def _compute_shipping_assignment(self):
+    #     for rec in self:
+    #         if rec.company_id.id == 4:
+    #             if rec.x_studio_canal_de_distribucin == 'INTERGRUPO':
+    #                 rec.shipping_assignment = 'shipments'
+    #                 continue
+    #             if rec.location_id and rec.location_id.warehouse_id.name:
+    #                 if any(keyword in rec.location_id.warehouse_id.name for keyword in ['Amazon', 'MercadoLibre']):
+    #                     rec.shipping_assignment = 'shipments'
+    #                     continue
+    #                 if rec.location_id.warehouse_id.name == 'Monterrey PR': 
+    #                     if rec.x_studio_canal_de_distribucin == 'DISTRIBUIDORES':
+    #                         rec.shipping_assignment = 'shipments'
+    #                         continue
+    #                     if rec.move_ids.filtered(lambda record: record.product_id.default_code == 'FLTENVIO'):
+    #                         rec.shipping_assignment = 'shipments'
+    #                         continue
+    #             rec.shipping_assignment= 'logistics'
+    #             continue
+    #         else:
+    #             rec.shipping_assignment = 'shipments'
 
     def _group_expand_states(self, states, domain, order):
         return [key for key, val in self._fields['kanban_task_status'].selection]
