605f99290c1454f7437e689cd1f6630b7201709d
David Morales <dmoralesh1989@gmail.com>
2024-12-18 18:26:03 -0700

    [FIX] tax index

--- xe_customs/models/account_move.py
+++ xe_customs/models/account_move.py
@@ -36,7 +36,9 @@ class AccountMove(models.Model):
             move.source_orders.down_payment_context = 0
             for order_line in move.invoice_line_ids.sale_line_ids:
                 if move.id in order_line.invoice_lines.move_id.ids:
-                    order_line.order_id.down_payment_context += order_line.price_unit * (1 + (order_line.tax_id[0].amount / 100))
+                    tax_id = order_line.taxes_id.sudo()
+                    total_tax = sum(tax_id.mapped('amount'))
+                    order_line.order_id.down_payment_context += order_line.price_unit * (1 + (total_tax / 100))
             move.reconciled_amount = sum(move.source_orders.mapped('down_payment_context'))
             move.reconcile_balance = sum(move.invoice_line_ids.filtered(lambda x: x.product_id.id == x.company_id.sale_down_payment_product_id.id).mapped('price_total')) - move.reconciled_amount
             move.has_down_payment = move.reconcile_balance > 0
