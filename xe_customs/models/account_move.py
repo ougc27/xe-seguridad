fe7eec4f762e8d28381bdacc64206449d0b5000c
David Morales <dmoralesh1989@gmail.com>
2024-12-18 17:30:42 -0700

    [ADD] mapped orders

--- xe_customs/models/account_move.py
+++ xe_customs/models/account_move.py
@@ -31,7 +31,7 @@ class AccountMove(models.Model):
     
     def _get_source_orders(self):
         for move in self:
-            move.source_orders = move.invoice_line_ids.mapped('sale_line_ids.order_id').order_id
+            move.source_orders = move.invoice_line_ids.mapped('sale_line_ids.order_id')
             move.source_orders.down_payment_context = 0
             for order_line in move.invoice_line_ids.sale_line_ids:
                 if move.id in order_line.invoice_lines.move_id.ids:
