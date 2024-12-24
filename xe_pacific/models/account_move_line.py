57937c28191e52cef104bb1779746003dbd9414b
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-24 02:03:22 -0600

    [FIX] Changes pacific in testing

--- xe_pacific/models/account_move_line.py
+++ xe_pacific/models/account_move_line.py
@@ -1,11 +1,31 @@
-from odoo import models, fields
+from odoo import fields, models, api
+from odoo.tools import frozendict
 
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
-
+    
     x_order_id = fields.Many2one('sale.order',
         related='sale_line_ids.order_id',
         store=True,
         readonly=True
     )
+
+    @api.depends('account_id', 'partner_id', 'product_id')
+    def _compute_analytic_distribution(self):
+        cache = {}
+        for line in self:
+            if line.display_type == 'product' or not line.move_id.is_invoice(include_receipts=True):
+                arguments = frozendict({
+                    "product_id": line.product_id.id,
+                    "product_categ_id": line.product_id.categ_id.id,
+                    "partner_id": line.partner_id.id,
+                    "partner_category_id": line.partner_id.category_id.ids,
+                    "account_prefix": line.account_id.code,
+                    "company_id": line.company_id.id,
+                    "warehouse_id": line.move_id.x_studio_almacen_id.id,
+                    "team_id": line.move_id.team_id.id
+                })
+                if arguments not in cache:
+                    cache[arguments] = self.env['account.analytic.distribution.model']._get_distribution(arguments)
+                line.analytic_distribution = cache[arguments] or line.analytic_distribution
