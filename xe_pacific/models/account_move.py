10bfce0489414ba57e8ba83589a93a5276b64e58
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2024-12-26 15:49:03 -0600

    [UPD] Warehouse changes

--- xe_pacific/models/account_move.py
+++ xe_pacific/models/account_move.py
@@ -1,14 +1,8 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-
-    x_order_id = fields.Many2one('sale.order',
-        related='invoice_line_ids.x_order_id',
-        store=True,
-        readonly=True
-    )
 
     pos_store = fields.Many2one('res.partner', 
         related='x_order_id.pos_store',
@@ -25,3 +19,35 @@ class AccountMove(models.Model):
         #context={'from_helpdesk_ticket': True},
         help='Relationship between referrals and invoice.'
     )
+
+    x_order_id = fields.Many2one('sale.order',
+        related='invoice_line_ids.x_order_id',
+        store=True,
+        readonly=True
+    )
+
+    x_studio_almacen_id = fields.Many2one(
+        'stock.warehouse',
+        'Warehouse',
+        compute='_compute_warehouse_id',
+        readonly=True,
+        store=True,
+        tracking=True
+    )
+
+    warehouse_id = fields.Many2one(
+        'stock.warehouse',
+        'Warehouse',
+    )
+
+    @api.depends('line_ids', 'x_order_id', 'warehouse_id')
+    def _compute_warehouse_id(self):
+        for record in self:
+            if record.warehouse_id:
+                record['x_studio_almacen_id'] = record.warehouse_id
+                continue
+            order_id = record.x_order_id
+            if order_id:
+                record['x_studio_almacen_id'] = order_id.warehouse_id.id
+            if record.purchase_order_count >= 1:
+                record['x_studio_almacen_id'] = record.line_ids[0].purchase_order_id.picking_type_id.warehouse_id.id
