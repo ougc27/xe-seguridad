153571fad13b78c2c6b4f8468213c73d8b2d7eb6
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-01-08 17:37:14 -0600

    [UPD] Analytic distribution new features

--- xe_pacific/models/purchase_order_line.py
+++ xe_pacific/models/purchase_order_line.py
@@ -8,26 +8,12 @@ class PurchaseOrderLine(models.Model):
     def _compute_analytic_distribution(self):
         for line in self:
             if not line.display_type:
-                initial_distribution = line.env['account.analytic.distribution.model']._get_distribution({
+                distribution = self.env['account.analytic.distribution.model']._get_distribution({
                     "product_id": line.product_id.id,
                     "product_categ_id": line.product_id.categ_id.id,
                     "partner_id": line.order_id.partner_id.id,
                     "partner_category_id": line.order_id.partner_id.category_id.ids,
                     "company_id": line.company_id.id,
-                })
-                
-                additional_distribution = line.env['account.analytic.distribution.model']._get_distribution({
-                    "company_id": line.company_id.id,
                     "warehouse_id": line.order_id.picking_type_id.warehouse_id.id
                 })
-
-                #if initial_distribution == additional_distribution:
-                    #line.analytic_distribution = initial_distribution or line.analytic_distribution
-    
-                combined_distribution = {}
-                for dist in [initial_distribution, additional_distribution]:
-                    if dist:
-                        for key, value in dist.items():
-                            combined_distribution[key] = combined_distribution.get(key, 0) + value
-                
-                line.analytic_distribution = combined_distribution or line.analytic_distribution
+                line.analytic_distribution = distribution or line.analytic_distribution
