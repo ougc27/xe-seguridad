53cc62a6b72b1b30a92e2e497328b3f109cab382
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-01-14 23:33:46 -0600

    [UPD] Stock picking sale note format

--- xe_pacific/models/account_move.py
+++ xe_pacific/models/account_move.py
@@ -40,6 +40,15 @@ class AccountMove(models.Model):
         'Warehouse',
     )
 
+    order_ids = fields.Many2many(
+        'sale.order',
+        'account_move_sale_order_rel',
+        'move_id',
+        'order_id',
+        string='Sale Orders',
+        compute='_compute_order_ids'
+    )
+
     @api.depends('line_ids', 'x_order_id', 'warehouse_id')
     def _compute_warehouse_id(self):
         for record in self:
@@ -51,3 +60,11 @@ class AccountMove(models.Model):
                 record['x_studio_almacen_id'] = order_id.warehouse_id.id
             if record.purchase_order_count >= 1:
                 record['x_studio_almacen_id'] = record.line_ids[0].purchase_order_id.picking_type_id.warehouse_id.id
+
+    @api.depends('invoice_origin')
+    def _compute_order_ids(self):
+        for record in self:
+            if record.invoice_origin:
+                order_names = [name.strip() for name in record.invoice_origin.split(',')]
+                record.order_ids = self.env['sale.order'].search([('name', 'in', order_names)]).ids
+            order_names = False
