df361dc6593458ff8d5d120f0c0b5f597a4e8f6c
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-01-17 10:54:56 -0600

    [UPD] Fix tax_id records in sale_order_line

--- xe_l10n_mx_edi/models/sale_order_line.py
+++ xe_l10n_mx_edi/models/sale_order_line.py
@@ -7,7 +7,6 @@ class SaleOrderLine(models.Model):
 
     @api.depends('order_partner_id')
     def _compute_tax_id(self):
-        # EXTENDS 'sale'
         super()._compute_tax_id()
         for line in self:
             partner = line.order_partner_id
@@ -15,5 +14,9 @@ class SaleOrderLine(models.Model):
                 partner = partner.parent_id
             if partner.is_border_zone_iva:
                 tax = self.env.ref('account.3_tax17', raise_if_not_found=False)
+                if line.company_id.id == 1:
+                    tax = self.env.ref('account.1_tax17', raise_if_not_found=False)
+                if line.company_id.id == 4:
+                    tax = self.env.ref('account.4_tax17', raise_if_not_found=False)
                 if tax:
                     line.tax_id = tax
