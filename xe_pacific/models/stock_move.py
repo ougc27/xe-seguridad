7c2a7b67ca4809dde7127668df1f9e6dcab30c71
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-02-12 10:09:59 -0600

    [FIX] Fix write method

--- xe_pacific/models/stock_move.py
+++ xe_pacific/models/stock_move.py
@@ -17,6 +17,6 @@ class StockMove(models.Model):
             if new_quantity != rec.quantity:
                 if rec.picking_id.state == "transit":
                     raise UserError(_("You cannot change the demanded quantity in transit state"))
-                if new_quantity > rec.move_id.product_uom_qty:
+                if new_quantity > rec.product_uom_qty:
                     raise UserError(_("You cannot update the quantity with a quantity greater than the demand"))
         return super().write(vals)
