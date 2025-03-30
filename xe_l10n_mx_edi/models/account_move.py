6bcf340bec8547e31ed9f321ac8d5021a6593496
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-03-30 17:47:08 -0600

    [ADD] Added warehouse_id value into invoice payment for getting the timezone of the customer.

--- xe_l10n_mx_edi/models/account_move.py
+++ xe_l10n_mx_edi/models/account_move.py
@@ -18,3 +18,18 @@ class AccountMove(models.Model):
              "value is defined by the customer.\nNote: It is not cause for cancellation if the key set is not the usage "
              "that will give the receiver of the document.",
     )
+
+    def l10n_mx_edi_cfdi_invoice_try_update_payments(self):
+        """ Try to update the state of payments for the current invoices. """
+        warehouse_id = self.x_studio_almacen_id
+        payments_diff = self._l10n_mx_edi_cfdi_invoice_get_payments_diff()
+
+        for invoice, commands in payments_diff['to_remove'].items():
+            invoice.l10n_mx_edi_invoice_document_ids = commands
+
+        for payment, pay_results in payments_diff['to_process']:
+            if not payment.x_studio_almacen_id:
+                payment.write({
+                    'warehouse_id': warehouse_id
+                })
+            payment.l10n_mx_edi_cfdi_invoice_try_update_payment(pay_results)
