7723035cd830b0bfe0e1f3f80527f329eaaf0716
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-05 15:30:31 -0600

    [ADD] xebrands_custom module added

---
+++ xebrands_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'XE Brands custom',
+    'version': '17.0',
+    'summary': 'XE Brands custom',
+    'description': 'XE Brands custom',
+    'author': 'Xe Brands',
+    'category': 'Sales/Sales',
+    'depends': [
+        'sale_management',
+        'stock',
+        'account_accountant',
+        'xe_pacific',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+        'views/bank_rec_widget_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ xebrands_custom/models/sale_order.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.depends('order_line')
+    def _compute_to_billing(self):
+        for record in self:
+            amount_to_billing = 0
+            dates = []
+            for line in record.order_line:
+                qty = line.qty_delivered - line.qty_invoiced
+                qty_pending = qty if qty > 0 else 0
+                qty_delivered = 0
+                if qty_pending:
+                    tax_results = record.env['account.tax'].with_company(line.company_id)._compute_taxes([
+                        line._convert_to_tax_base_line_dict_xebrands(qty_pending)
+                    ])
+                    totals = list(tax_results['totals'].values())[0]
+                    amount_untaxed = totals['amount_untaxed']
+                    amount_tax = totals['amount_tax']
+                    amount_to_billing += amount_untaxed + amount_tax
+
+                    if record.team_id == record.env.ref('__export__.crm_team_6_c580c7a3'):
+                        invoice_picking_ids = record.invoice_ids.picking_ids
+                        picking_ids = record.picking_ids.filtered(
+                            lambda o: o.state == 'done' and
+                                      o.id not in invoice_picking_ids.ids
+                        ).sorted(lambda o: o.date_done)
+                        for picking_id in picking_ids:
+                            dates.append(picking_id.date_done)
+                    else:
+                        picking_ids = record.picking_ids.filtered(
+                            lambda o: o.state == 'done'
+                        ).sorted(lambda o: o.date_done)
+                        for picking_id in picking_ids:
+                            move_id = picking_id.move_ids_without_package.filtered(
+                                lambda o: o.product_id == line.product_id
+                            )
+                            if move_id:
+                                qty_delivered += move_id.quantity
+                                if qty_delivered > line.qty_invoiced:
+                                    if picking_id.date_done not in dates:
+                                        dates.append(picking_id.date_done)
+                                    break
+            if dates:
+                dates = sorted(dates)
+                record.picking_date_to_billing = dates[0]
+            else:
+                record.picking_date_to_billing = False
+            record.amount_to_billing = amount_to_billing
+            record.billing_pending = bool(amount_to_billing)
+
+    billing_pending = fields.Boolean(
+        string='Pending billing',
+        compute='_compute_to_billing',
+    )
+    amount_to_billing = fields.Monetary(
+        string='Amount to billing',
+    )
+    picking_date_to_billing = fields.Datetime(
+        string='Picking date to billing',
+    )
