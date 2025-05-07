b0f0aa63602396cf344d65f602329c86683331ff
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-07 00:20:31 -0600

    [ADD] Add can merge contacts group

--- xe_pacific/models/hr_employee.py
+++ xe_pacific/models/hr_employee.py
@@ -5,3 +5,9 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     employee_number = fields.Integer()
+
+
+class HrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    employee_number = fields.Integer(related='employee_id.employee_number', readonly=True)
