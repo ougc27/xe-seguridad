55c63442ea368e6b85c498593fd41d0a17240b10
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-16 09:35:27 -0600

    [ADD Xe brands custom modulo into main

---
+++ xebrands_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'XE Brands custom',
+    'version': '17.0',
+    'summary': 'XE Brands custom',
+    'description': 'XE Brands custom',
+    'author': 'Xe Brands',
+    'category': 'Sales/Sales',
+    'depends': [
+        'xe_pacific',
+        'account_accountant',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+        'views/bank_rec_widget_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
