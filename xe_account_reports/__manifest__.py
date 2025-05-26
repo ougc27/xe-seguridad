65cbea40b744a962c4ac283cf479ba3d68dcdaea
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-26 08:56:17 -0600

    [ADD] Xe account reports module for adding sales team in reports

---
+++ xe_account_reports/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Accounting Reports",
+    "version": "17.0.0.0.1",
+    'license': 'LGPL-3',
+    "author": "XE BRANDS",
+    'sequence': 1,
+    "depends": ['account_reports'],
+    'category': 'Account/Reports',
+    'summary': 'Account Reports',
+    "data": [
+        'data/aged_partner_balance.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
