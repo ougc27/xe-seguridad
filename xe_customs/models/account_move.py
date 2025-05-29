1f543c9203ea3d68f493ddb59309c90c4313e7b2
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-28 19:55:42 -0600

    [ADD] Argument types in field

--- xe_customs/models/account_move.py
+++ xe_customs/models/account_move.py
@@ -11,9 +11,9 @@ class AccountMove(models.Model):
 
     source_orders = fields.Many2many(
         comodel_name='sale.order',
-        'account_move_sale_order_rel',
-        'move_id',
-        'sale_order_id ',
+        relation='account_move_sale_order_rel',
+        column1='move_id',
+        column2='sale_order_id',
         string="Invoices",
         compute='_get_source_orders',
         store=True
