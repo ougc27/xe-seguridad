c09c54ae1272a4aebdf6dff842bba2dde13470c7
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-05-29 15:20:08 -0600

    [UPD] Remove computed fields

--- commissions/models/res_partner.py
+++ commissions/models/res_partner.py
@@ -7,24 +7,6 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    def _compute_is_commission_user(self):
-        self.is_commission_user = self.env.user.has_group('commissions.commissions_user_group') and not self.env.user.has_group('commissions.advanced_user_commissions_group')
-
-    def _compute_is_advanced_commission_user(self):
-        self.is_advanced_commission_user = self.env.user.has_group('commissions.advanced_user_commissions_group') and not self.env.user.has_group('commissions.commissions_payment_group')
-
-    def _compute_is_commission_admin(self):
-        self.is_commission_admin = self.env.user.has_group('commissions.commission_admin_group')
-
-    def _default_is_commission_user(self):
-        return self.env.user.has_group('commissions.commissions_user_group') and not self.env.user.has_group('commissions.advanced_user_commissions_group')
-
-    def _default_is_advanced_commission_user(self):
-        return self.env.user.has_group('commissions.advanced_user_commissions_group') and not self.env.user.has_group('commissions.commissions_payment_group')
-
-    def _default_is_commission_admin(self):
-        return self.env.user.has_group('commissions.commission_admin_group')
-
     agent1_id = fields.Many2one(
         comodel_name='xe.agent',
         string='Agent 1',
@@ -40,21 +22,6 @@ class ResPartner(models.Model):
     agent2_per = fields.Float(
         string='Agent 2 percentage',
         copy=False,
-    )
-    is_commission_user = fields.Boolean(
-        string='Is commission user',
-        compute='_compute_is_commission_user',
-        default=_default_is_commission_user,
-    )
-    is_advanced_commission_user = fields.Boolean(
-        string='Is advanced commission user',
-        compute='_compute_is_advanced_commission_user',
-        default=_default_is_advanced_commission_user,
-    )
-    is_commission_admin = fields.Boolean(
-        string='Is commission administrator',
-        compute='_compute_is_commission_admin',
-        default=_default_is_commission_admin,
     )
 
     @api.onchange('agent1_id')
