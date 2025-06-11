413d5dc27f797542664450aa0ecd85b934c52819
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-06-11 01:51:23 -0600

    [ADD] Helpdesk changes creation ticket from picking

---
+++ xe_pacific/models/helpdesk_team.py
@@ -0,0 +1,70 @@
+from odoo import fields, models, api
+from odoo.osv import expression
+
+
+class HelpdeskTeam(models.Model):
+    _inherit = 'helpdesk.team'
+
+    total_tickets = fields.Integer(string='Total Tickets', compute='_compute_total_tickets')
+    active_tickets = fields.Integer(compute='_compute_active_tickets')
+    inactive_tickets = fields.Integer(compute='_compute_inactive_tickets')
+
+    def _compute_ticket_closed(self):
+        ticket_data = self.env['helpdesk.ticket']._read_group([
+            ('team_id', 'in', self.ids),
+            ('stage_id.name', 'ilike', 'Finalizado')],
+            ['team_id'], ['__count'])
+        mapped_data = {team.id: count for team, count in ticket_data}
+        for team in self:
+            team.ticket_closed = mapped_data.get(team.id, 0)
+
+    def _compute_total_tickets(self):
+        for team in self:
+            team.total_tickets = len(team.ticket_ids)
+
+    def _compute_active_tickets(self):
+        ticket_data = self.env['helpdesk.ticket']._read_group([
+            ('team_id', 'in', self.ids), ('stage_id.name', 'not ilike', 'Inactivo'), ('stage_id.name', 'not ilike', 'Finalizado')
+        ], ['team_id'], ['__count'])
+        mapped_data = {team.id: count for team, count in ticket_data}
+        for team in self:
+            team.active_tickets = mapped_data.get(team.id, 0)
+        
+    def _compute_inactive_tickets(self):
+        ticket_data = self.env['helpdesk.ticket']._read_group([
+            ('team_id', 'in', self.ids), ('stage_id.name', 'ilike', 'Inactivo')
+        ], ['team_id'], ['__count'])
+        mapped_data = {team.id: count for team, count in ticket_data}
+        for team in self:
+            team.inactive_tickets = mapped_data.get(team.id, 0)
+
+    def action_view_active_tickets(self):
+        action = self.action_view_ticket()
+        action.update({
+            'domain': [
+                ('team_id', '=', self.id),
+                ('stage_id.name', 'not ilike', 'Inactivo'),
+                ('stage_id.name', 'not ilike', 'Finalizado')
+            ]
+        })
+        return action
+
+    def action_view_inactive_ticket(self):
+        action = self.action_view_ticket()
+        action.update({
+            'domain': [
+                ('team_id', '=', self.id),
+                ('stage_id.name', 'ilike', 'Inactivo'),
+            ]
+        })
+        return action
+
+    def action_view_closed_ticket(self):
+        action = self.action_view_ticket()
+        action.update({
+            'domain': [
+                ('team_id', '=', self.id),
+                ('stage_id.name', 'ilike', 'Finalizado'),
+            ]
+        })
+        return action
--- xe_pacific/wizard/generate_ticket_wizard.py
+++ xe_pacific/wizard/generate_ticket_wizard.py
@@ -13,17 +13,16 @@ class TicketTransferWizard(models.TransientModel):
     ticket_type = fields.Selection([
         ('paint', 'Paint'),
         ('functionality', 'Functionality')], required=True)
-    ticket_name = fields.Char(required=True)
+    team_id = fields.Many2one('helpdesk.team', string='Helpdesk Team', domain="[('company_id', '=', company_id)]", required=True)
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
 
     def action_validate_ticket(self):
         picking_id = self.picking_id
-        team_id = self.env['helpdesk.team'].search([('name', 'ilike', 'Atención al cliente')]).id
         ticket_type_name = 'pintura' if self.ticket_type == 'paint' else 'funcionamiento'
         ticket_type_id = self.env['helpdesk.ticket.type'].search([('name', 'ilike', ticket_type_name)]).id
         ticket_vals = {
             'picking_id': picking_id.id,
-            'name': self.ticket_name,
-            'team_id': team_id,
+            'team_id': self.team_id.id,
             'ticket_type_id': ticket_type_id,
             'user_id': self.user_id.id,
             'partner_id': self.partner_id.id,
