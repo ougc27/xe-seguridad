c204f120a21985829359ff881ad4965f9324cd46
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-10-31 09:21:10 -0600

    [FIX] Correct version in storage modules

--- cloud_storage/__manifest__.py
+++ cloud_storage/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Cloud Storage",
     "summary": """Store chatter attachments in the cloud""",
     "category": "Technical Settings",
-    "version": "1.0",
+    "version": "17.0.0.0.0",
     "depends": ["mail"],
     "data": [
         "views/settings.xml",
