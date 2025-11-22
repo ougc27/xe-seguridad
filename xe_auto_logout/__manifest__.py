71918b92324c902f530eae3e3a7727a619630467
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2025-11-21 18:15:30 -0600

    [ADD] XE auto logout module on night sundays

---
+++ xe_auto_logout/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'XE Auto Logout',
+    'version': '17.0.0.0.2',
+    'category': 'Extra Tools',
+    'summary': """Auto logout idle user with fixed time on sundays""",
+    'description': """If the user
+     is in idle mode the user will logout from session automatically """,
+    "license": "LGPL-3",
+    "author": "XE Brands",
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'xe_auto_logout/static/src/xml/auto_logout_biweekly.xml',
+            'xe_auto_logout/static/src/js/auto_logout_biweekly.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
