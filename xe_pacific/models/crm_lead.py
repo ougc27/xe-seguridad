0b6cd0a1890aeafeacb6809fd97be77c6c4131fd
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2026-01-27 00:56:55 -0600

    [ADD] Re-assign conversations to leader

--- xe_pacific/models/crm_lead.py
+++ xe_pacific/models/crm_lead.py
@@ -11,3 +11,19 @@ class Lead(models.Model):
         ('gott', 'XE Brands'),
         ('protecto_home', 'Romax Comercial SA de CV'),
     ], string='Vambe Brands')
+
+    def write(self, vals):
+        if 'user_id' in vals and vals.get('user_id') is False:
+            for rec in self:
+                if rec.vambe_brands:
+                    self.env['ir.logging'].sudo().create({
+                        'name': 'Vambe Lead user_id set to NULL',
+                        'type': 'server',
+                        'dbname': self.env.cr.dbname,
+                        'level': 'WARNING',
+                        'message': f'user_id wiped. vals={vals}',
+                        'path': 'crm.lead',
+                        'func': 'write',
+                        'line': '15',
+                    })
+        return super().write(vals)
