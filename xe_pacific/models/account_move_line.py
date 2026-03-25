6f7e9e205c45ad8ec85f4d0cd557a81cc69ccf0a
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2026-03-25 01:00:31 -0600

    [ADD] Raise User Error especification

--- xe_pacific/models/account_move_line.py
+++ xe_pacific/models/account_move_line.py
@@ -2,8 +2,6 @@ from odoo import fields, models, api, _
 from odoo.tools import frozendict
 from odoo.exceptions import UserError
 
-import logging
-_logger = logging.getLogger()
 
 
 class AccountMoveLine(models.Model):
@@ -49,6 +47,38 @@ class AccountMoveLine(models.Model):
                 if arguments not in cache:
                     cache[arguments] = self.env['account.analytic.distribution.model']._get_distribution(arguments)
                 line.analytic_distribution = cache[arguments] or line.analytic_distribution
+
+
+    def _check_amls_exigibility_for_reconciliation(self, shadowed_aml_values=None):
+        """ Ensure the current journal items are eligible to be reconciled together.
+        :param shadowed_aml_values: A mapping aml -> dictionary to replace some original aml values to something else.
+                                    This is usefull if you want to preview the reconciliation before doing some changes
+                                    on amls like changing a date or an account.
+        """
+        if not self:
+            return
+
+        if any(aml.reconciled for aml in self):
+            raise UserError(_("You are trying to reconcile some entries that are already reconciled. %s") % (self.id, self.move_id))
+        if any(aml.parent_state != 'posted' for aml in self):
+            raise UserError(_("You can only reconcile posted entries."))
+        accounts = self.mapped(lambda x: x._get_reconciliation_aml_field_value('account_id', shadowed_aml_values))
+        if len(accounts) > 1:
+            raise UserError(_(
+                "Entries are not from the same account: %s",
+                ", ".join(accounts.mapped('display_name')),
+            ))
+        if len(self.company_id.root_id) > 1:
+            raise UserError(_(
+                "Entries don't belong to the same company: %s",
+                ", ".join(self.company_id.mapped('display_name')),
+            ))
+        if not accounts.reconcile and accounts.account_type not in ('asset_cash', 'liability_credit_card'):
+            raise UserError(_(
+                "Account %s does not allow reconciliation. First change the configuration of this account "
+                "to allow it.",
+                accounts.display_name,
+            ))
 
     """def _check_amls_exigibility_for_reconciliation(self, shadowed_aml_values=None):
         if not self:
