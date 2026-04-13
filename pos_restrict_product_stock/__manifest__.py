31e7910f75c8b517667c29b5e6be92d8185389ef
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2026-04-13 16:01:09 -0600

    [ADD] Has payment proof field added into pos.orders

--- pos_restrict_product_stock/__manifest__.py
+++ pos_restrict_product_stock/__manifest__.py
@@ -20,7 +20,7 @@
 #############################################################################
 {
     'name': 'Display Stock in POS | Restrict Out-of-Stock Products in POS',
-    'version': '17.0.1.0.59',
+    'version': '17.0.1.0.60',
     'category': 'Point of Sale',
     'summary': """Enhance your Point of Sale experience by preventing the 
     ordering of out-of-stock products during your session""",
