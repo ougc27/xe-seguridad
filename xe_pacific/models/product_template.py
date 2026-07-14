284559db909944fe401f071c2f635db40b7bf59e
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2026-07-14 00:50:51 -0600

    [ADD] Prevent return products

--- xe_pacific/models/product_template.py
+++ xe_pacific/models/product_template.py
@@ -5,3 +5,11 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     not_automatic_lot_number = fields.Boolean()
+
+    block_return = fields.Boolean(
+        string="Blocked for Returns",
+        copy=False,
+        readonly=True,
+        help="While enabled, returns of this product cannot be validated. "
+             "Set automatically by purchase receptions flagged as blocked.",
+    )
