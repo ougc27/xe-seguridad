8aec04cfc7de0e98c2e6fa7eb95c39a1ebc7583c
Rogelio Gonzalez <sistemas4@xeseguridad.com>
2026-08-25 00:48:48 -0600

    fix(xe_pacific): remove migration code

--- xe_pacific/migrations/17.0.0.0.79/post-migrate.py
+++ xe_pacific/migrations/17.0.0.0.79/post-migrate.py
@@ -9,5 +9,5 @@ def migrate(cr, version):
     """
     if not version:
         return
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['calendar.event'].sudo().search([('opportunity_id', '!=', False)])._sync_tags_from_opportunity()
+    #env = api.Environment(cr, SUPERUSER_ID, {})
+    #env['calendar.event'].sudo().search([('opportunity_id', '!=', False)])._sync_tags_from_opportunity()
